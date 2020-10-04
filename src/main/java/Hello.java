19f620c6f00b8e4ef0747415781690da2220c273
igorn12 <igor.nasciment@gmail.com>
2020-10-04 03:13:22 -0300

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "Hello", urlPatterns = "/hello")
+public class Hello extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("deu certo");
+
+        ServletContext servletContext = getServletContext();
+
+        List<Usuario> usuarioList = (ArrayList<Usuario>) servletContext.getAttribute("usuarios");
+
+        for (Usuario u : usuarioList) {
+            response.getWriter().println(u.toString());
+        }
+        
+    }
+}
---
+++ src/main/java/InicializaListaUsuarios.java
@@ -0,0 +1,41 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import java.rmi.server.ServerCloneException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebListener()
+public class InicializaListaUsuarios implements ServletContextListener{
+
+    public InicializaListaUsuarios() {
+    }
+
+    public void contextInitialized(ServletContextEvent sce) {
+        Usuario u1 = new Usuario(1, "Igor", "igor@gmail.com", "12345");
+        Usuario u2 = new Usuario(2, "Ana", "ana@gmail.com", "11111");
+        Usuario u3 = new Usuario(3, "Gustavo", "gustavo@gmail.com", "22222");
+        Usuario u4 = new Usuario(4, "Cecilia", "cecilia@gmail.com", "33333");
+
+        List<Usuario> usuariosList = new ArrayList<>();
+
+        usuariosList.add(u1);
+        usuariosList.add(u2);
+        usuariosList.add(u3);
+        usuariosList.add(u4);
+
+        ServletContext servletContext = sce.getServletContext();
+        servletContext.setAttribute("usuarios", usuariosList);
+
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+
+}
